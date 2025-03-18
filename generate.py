528b992eb049cf95853b1c3ee918a7bfa6b44f44
antjuntu <antjuntu@users.noreply.github.com>
2025-03-18 11:40:17 +0000

    Tue, Mar 18, 2025, 1:40 PM +02:00

--- generate.py
+++ generate.py
@@ -168,10 +168,12 @@ class CrosswordCreator():
         return len(assignment) == len(self.crossword.variables)
 
     def consistent(self, assignment):
+        #consistent identifies when assignment doesn't meet unary constraints
         """
         Return True if `assignment` is consistent (i.e., words fit in crossword
         puzzle without conflicting characters); return False otherwise.
         """
+        
         for (x, y) in self.crossword.overlaps:
             if self.crossword.overlaps[x,y] and (x in assignment) and (y in assignment):
                 i, j = self.crossword.overlaps[x,y]
